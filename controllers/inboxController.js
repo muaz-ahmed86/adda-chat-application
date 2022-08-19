229f883c4c91987f63dc5c49679c263cb4ca9952
Md Muaz Ahmed <mdmuazahmed86@gmail.com>
2022-08-19 21:47:19 +0600

    have finished creating addConversation controller

--- controllers/inboxController.js
+++ controllers/inboxController.js
@@ -1,5 +1,11 @@
+// external imports
+const createError = require('http-errors');
+
 // internal imports
-const Conversation = require('../models/Conversation')
+const Conversation = require('../models/Conversation');
+const User = require('../models/User');
+
+const escape = require('../utils/escape')
 
 const getInbox = async (req, res, next) => {
     try {
@@ -18,6 +24,71 @@ const getInbox = async (req, res, next) => {
     }
 }
 
+const searchUsers = async (req, res, next) => {
+    const searchKeyword = req.body.searchKeyword;
+    const searchQuery = searchKeyword.replace('+88', "");
+
+    const name_search_regex = new RegExp(escape(searchQuery), "i");
+    const email_search_regex = new RegExp("^" + escape("+88" + searchQuery));
+    const mobile_search_regex = new RegExp("^" + escape(searchQuery) + "$", "i");
+
+    try {
+        if(searchQuery !== "") {
+            const users = await User.find({
+                $or: [
+                    {name: name_search_regex},
+                    {email: email_search_regex},
+                    {mobile: mobile_search_regex}
+                ]
+            })
+            res.json(users)
+        } else {
+            throw createError('Search input can not be empty!')
+        }  
+    } catch (error) {
+        res.status(500).json({
+            errors: {
+                common: {
+                    msg: error.message
+                }
+            }
+        })
+    }
+}
+
+const addConversation = async (req, res, next) => {
+    try {
+        const isConversationExisted = await Conversation.find({
+            creator: req.user.id,
+            participant: req.body.id
+        })
+    
+        if(isConversationExisted.length === 0) {
+            const newConversation = new Conversation({
+                creator: req.user.id,
+                participant: req.body.id
+            })
+    
+            const result = await newConversation.save();
+            res.status(200).json({
+                message: 'Conversation has been added successfully!'
+            })
+        } else {
+            throw createError('User already added to your conversation')
+        }   
+    } catch (error) {
+        res.status(500).json({
+            errors: {
+                common: {
+                    msg: error.message
+                }
+            }
+        })
+    }
+}
+
 module.exports = {
-    getInbox
+    getInbox,
+    searchUsers,
+    addConversation
 }
--- routes/inboxRouter.js
+++ routes/inboxRouter.js
@@ -2,11 +2,14 @@
 const router = require('express').Router();
 
 // internal imports
-const { getInbox } = require('../controllers/inboxController');
+const { getInbox, searchUsers, addConversation } = require('../controllers/inboxController');
 const decorateHtmlResponse = require('../middlewares/commons/decorateHtmlResponse');
 const { isLoggedIn } = require('../middlewares/commons/isLoggedIn')
 
 // routes
 router.get('/', decorateHtmlResponse("Inbox"), isLoggedIn, getInbox);
+
+router.post('/search', isLoggedIn, searchUsers);
+router.post('/conversation', isLoggedIn, addConversation);
 
 module.exports = router;
